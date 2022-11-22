22678f47b7ec0712be6d427c10a5af5cb6a674bf
sdclayton <96846005+sdclayton@users.noreply.github.com>
2022-11-21 20:24:21 -0600

    add (routing for job-posting)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { JobPostingComponent } from './job-posting/job-posting.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/job-posting', pathMatch: 'full'},
+  {path: 'job-posting', component: JobPostingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
