c8f6e89a43518af5b3bcb9c855561e46fd61afe5
sdclayton <96846005+sdclayton@users.noreply.github.com>
2022-11-28 19:28:22 -0600

    add: job-posting.service & httpClient to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { JobPostingService } from './job-posting/job-posting.service';
 import { JobPostingComponent } from './job-posting/job-posting.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -16,9 +18,10 @@ import { JobPostingComponent } from './job-posting/job-posting.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [JobPostingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
