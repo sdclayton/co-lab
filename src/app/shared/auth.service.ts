188bf0bf7ef37aed7bd01a4b9f942a43090bfa63
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-12-01 15:50:09 -0600

    Update: Began creating and storing user data

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpErrorResponse } from "@angular/common/http"
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+
+
+import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 //Firebase credentials
 const SIGNUP_KEY = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC0-ebfD9PLUPURd4_HS8YIegHCpg97ASE';
@@ -22,7 +28,9 @@ export interface AuthResponseData {
 })
 export class AuthService{
 
-  constructor(private http: HttpClient){}
+  user = new Subject<User>();
+
+  constructor(private http: HttpClient, private router: Router){}
 
   signup(email: string, password: string){
     return this.http.post<AuthResponseData>(SIGNUP_KEY,
@@ -30,7 +38,21 @@ export class AuthService{
         email: email,
         password: password,
         returnSecureToken: true
-      })
+      }).pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        })
+      )
+  }
+
+  storeUser(firstName: string, lastName: string){
+
   }
 
   login(email: string, password: string){
@@ -39,6 +61,52 @@ export class AuthService{
         email: email,
         password: password,
         returnSecureToken: true
-      })
+      }).pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          )
+        })
+      )
   }
+
+  logout(){
+    this.user.next(null);
+    this.router.navigate(['/login'])
+  }
+
+  handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number){
+      const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      const user = new User(email, userId, token, expirationDate);
+      this.user.next(user);
+      // this.autoLogout(expiresIn * 1000);
+      // localStorage.setItem('userData', JSON.stringify(user));
+  }
+
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred'
+    if(!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch(errorRes.error.error.message){
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+    return throwError(errorMessage);
+    }
 }
--- src/app/shared/user.model.ts
+++ src/app/shared/user.model.ts
@@ -1,8 +1,8 @@
 export class User{
   constructor(
     public email: string,
-    public password: string,
-    private id: string,
+    // public password: string,
+    public id: string,
     private _token: string,
     private _tokenExpirationDate: Date ){}
 
