3954f8a9dd2de11747dc1a4da4e7574c73121db1
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-12-04 22:51:01 -0600

    Update: Fixed Routing on several components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JobPostingComponent } from './job-posting/job-posting.component';
 
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full' },
   {path: 'signup', component: SignUpComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'jobposting', component: JobPostingComponent},
+
 ];
 
 @NgModule({
