cd0e252c475209d8b2bae53b5a8228994427147d
emilyzimmerman <111322180+emilyzimmerman@users.noreply.github.com>
2022-12-08 19:38:44 -0600

    Revert "Discussion"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,36 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { JobPostingComponent } from './job-posting/job-posting.component';
-import { DiscussionComponent } from './discussion/discussion.component';
-import { DiscussionCardComponent } from './discussion/discussion-card/discussion-card.component';
-import { FormsModule } from '@angular/forms';
-import { DiscussionPostComponent } from './discussion/discussion-post/discussion-post.component';
-import { PostDetailComponent } from './discussion/post-detail/post-detail.component';
-import { DiscussionShowComponent } from './discussion/discussion-show/discussion-show.component';
-import { ReplyFormComponent } from './discussion/discussion-show/reply-form/reply-form.component';
-import { ReplyCardComponent } from './discussion/discussion-show/reply-card/reply-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    JobPostingComponent,
-    DiscussionComponent,
-    DiscussionCardComponent,
-    DiscussionPostComponent,
-    PostDetailComponent,
-    DiscussionShowComponent,
-    ReplyFormComponent,
-    ReplyCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/discussion/discussion.model.ts
+++
@@ -1,7 +0,0 @@
-export class Discussion {
-  constructor(
-    public title: string,
-    public postText: string,
-    public id: number,
-    public comment?: Comment[]) {}
-}
