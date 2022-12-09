70b5c1435297a8409119968b6ab597b231300782
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-12-08 19:45:25 -0600

    fix(routes)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,13 @@ import { DiscussionShowComponent } from './discussion/discussion-show/discussion
 import { DiscussionComponent } from './discussion/discussion.component';
 import { PostDetailComponent } from './discussion/post-detail/post-detail.component';
 
+
+
+import { JobPostingComponent } from './job-posting/job-posting.component';
+
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 const routes: Routes = [
   {
     path: 'discussion',
@@ -15,15 +22,6 @@ const routes: Routes = [
     path: 'discussion/:id',
     component: DiscussionShowComponent,
   },
-];
-
-
-import { JobPostingComponent } from './job-posting/job-posting.component';
-
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-
-const routes: Routes = [
   {path: 'signup', component: SignUpComponent},
   {path: 'login', component: LoginComponent},
   {path: 'job-posting', component: JobPostingComponent}
