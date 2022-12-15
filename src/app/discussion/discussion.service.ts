fe4cfa4b1cb54ddd34bb82e8b5545fb9c0d5a502
emilyzimmerman <zimmermanemily121@gmail.com>
2022-12-15 14:46:43 -0600

    ADD MORE EXAMPLES
    Added more examples to search through for demonstration

--- src/app/discussion/discussion.service.ts
+++ src/app/discussion/discussion.service.ts
@@ -23,6 +23,20 @@ export class DiscussionService {
         'Tri-tip pork belly shoulder capicola pork picanha flank. Ribeye meatball corned beef bresaola sausage pancetta alcatra tenderloin. Strip steak ground round ball tip bresaola landjaeger, andouille filet mignon leberkas ham. Ground round hamburger turducken porchetta chislic bacon rump short loin sausage biltong pancetta shoulder burgdoggen cupim.',
       comment: [],
     },
+    {
+      id: 2,
+      title: 'When will the sun explode?',
+      postText:
+        'The amber droplet hung from the branch, reaching fullness and ready to drop. It waited. While many of the other droplets were satisfied to form as big as they could and release, this droplet had other plans. It wanted to be part of history. It wanted to be remembered long after all the other droplets had dissolved into history. So it waited for the perfect specimen to fly by to trap and capture that it hoped would eventually be discovered hundreds of years in the future.',
+      comment: [],
+    },
+    {
+      id: 3,
+      title: 'Where do I get a pet dragon?',
+      postText:
+        "The river slowly meandered through the open space. It had hidden secrets that it didn't want to reveal. It had a well-planned strategy to appear calm, inviting, and appealing. That's how the river lured her unknowing victims to her water's edge.",
+      comment: [],
+    },
   ];
 
   constructor() {}
